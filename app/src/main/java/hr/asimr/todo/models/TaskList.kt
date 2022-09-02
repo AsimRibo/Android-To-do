62059756f1df01d44502f3e1c8587d72198b05ba
Asim Ribo <asim.ribo21@gmail.com>
2022-09-02 18:01:16 +0200

    Add TaskList class

---
+++ app/src/main/java/hr/asimr/todo/models/TaskList.kt
@@ -0,0 +1,14 @@
+package hr.asimr.todo.models
+
+import android.content.Context
+
+class TaskList(private val context: Context) : ArrayList<Task>() {
+
+    fun saveInFile(){
+
+    }
+
+    fun readFromFile(){
+        
+    }
+}
