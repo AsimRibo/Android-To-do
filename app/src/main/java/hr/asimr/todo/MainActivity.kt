e05f63d5575916ab8ab00b0003eafa27b59166ea
Asim Ribo <asim.ribo21@gmail.com>
2022-09-04 13:38:14 +0200

    Add button listeners and bottom sheet opening

--- app/src/main/java/hr/asimr/todo/MainActivity.kt
+++ app/src/main/java/hr/asimr/todo/MainActivity.kt
@@ -2,7 +2,9 @@ package hr.asimr.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import hr.asimr.todo.databinding.ActivityMainBinding
+import hr.asimr.todo.databinding.DialogAddTaskBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,5 +13,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+        initButtonListeners()
+    }
+
+    private fun initButtonListeners() {
+        binding.btnAdd.setOnClickListener{
+            val dialog = BottomSheetDialog(this)
+            val bottomSheetBinding = DialogAddTaskBinding.inflate(layoutInflater)
+
+            dialog.setContentView(bottomSheetBinding.root)
+
+            bottomSheetBinding.btnAddTaskDetails.setOnClickListener{
+                bottomSheetBinding.etTask.text?.clear()
+            }
+
+            dialog.show()
+        }
     }
 }
